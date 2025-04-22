1f827cf683619898ca12b68b7e95a39080e400ea
josefrancelino <jef.br@outlook.com>
2025-04-22 18:29:15 -0300

    Adicionado index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="css/w3.css">
+
+	<script type="text/javascript" src="js/htmx.org2.0.4.js"></script>
+
+	<title>MaoNaObra</title>
+</head>
+<body>
+
+	<header></header>
+
+	<nav></nav>
+
+	<section>
+		
+		<article>
+
+			<br>Bem vindo!
+			
+			<aside></aside>
+		</article>
+	</section>
+
+	<footer></footer>
+
+</body>
+</html>
