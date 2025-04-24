0538c382acfe4ac011012a13a7308cf5521af4c2
josefrancelino <jef.br@outlook.com>
2025-04-24 09:09:05 -0300

    Modificado frontend um novo branch

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" type="text/css" href="css/w3.css">
-
 	<script type="text/javascript" src="js/htmx.org2.0.4.js"></script>
 
 	<title>MaoNaObra</title>
